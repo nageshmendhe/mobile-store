ae307c9ccd2a67ac9e103ccf696fd6f486a0e35f
Nagesh Mendhe <mendhenagesh536@gmail.com>
2024-07-03 23:24:38 +0530

    added product controller

---
+++ mobile-store/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace mobile_store.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult ReturnHello()
+        {
+            return Ok("Hello");
+        }
+    }
+}
